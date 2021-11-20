f8c23e56c4fe6b568b87aa31382b38b8d1c1ad14
Mohamed Osama <mohamed.osama.swe@gmail.com>
2021-11-20 15:48:41 +0200

    update token

--- src/app/services/products-from-api.service.ts
+++ src/app/services/products-from-api.service.ts
@@ -11,7 +11,7 @@ export class ProductsFromAPIService {
 
   private httpOptions={};
   constructor(private httpClient: HttpClient) {
-    localStorage.setItem('AccessToken','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxOGViNGE1ZTRlMjZlN2Y2MjVjYTIyNiIsImlzQWRtaW4iOnRydWUsImlhdCI6MTYzNzAyMzczNiwiZXhwIjoxNjM3MjgyOTM2fQ.rErePjoBJpU2FrU1-_21ZTZubV0CurzFRFr4PNJ4HYM')
+    // localStorage.setItem('AccessToken','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxOGViNGE1ZTRlMjZlN2Y2MjVjYTIyNiIsImlzQWRtaW4iOnRydWUsImlhdCI6MTYzNzAyMzczNiwiZXhwIjoxNjM3MjgyOTM2fQ.rErePjoBJpU2FrU1-_21ZTZubV0CurzFRFr4PNJ4HYM')
     this.httpOptions = {
       headers: new HttpHeaders({
         'Content-Type': 'application/json'
