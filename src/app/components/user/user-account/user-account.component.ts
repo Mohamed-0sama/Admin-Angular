25479e595117a8da880accae9e18d43dd6c989e0
Abdelmomen-Nasreldin <92486538+Abdelmomen-Nasreldin@users.noreply.github.com>
2021-11-22 14:30:51 +0200

    responsive_v1

--- src/app/components/user/user-account/user-account.component.ts
+++ src/app/components/user/user-account/user-account.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, ElementRef, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { HostListener } from "@angular/core";
+import { MatDrawerMode } from '@angular/material/sidenav';
+// import { ElementMatcher } from '@angular/flex-layout';
 @Component({
   selector: 'app-user-account',
   templateUrl: './user-account.component.html',
@@ -7,11 +9,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserAccountComponent implements OnInit {
   isOpen:boolean = true;
-  constructor() { }
+  mode! : MatDrawerMode
+  screenHeight!: number;
+    screenWidth!: number;
+
+    constructor() {
+        this.getScreenSize();
+    }
+
+  ngOnChanges(changes: SimpleChanges): void {
+// if(nativeElement.width == 1200)
+  }
 
   ngOnInit(): void {
   }
   toogleAside(){
     this.isOpen = !this.isOpen;
   }
+  @HostListener('window:resize', ['$event'])
+    getScreenSize() : void {
+          this.screenHeight = window.innerHeight;
+          this.screenWidth = window.innerWidth;
+          console.log(this.screenHeight, this.screenWidth);
+          // if (this.screenWidth < 768) {
+          //   this.mode = "over"
+          // // }else if(this.screenWidth < 992){
+          // //   this.mode = "push"
+          // } else {
+          //   this.mode = "push"
+          // }
+          this.mode = "over"
+    }
+
 }
